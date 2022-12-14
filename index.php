53547f947810885c0bf68e4d69d443c12901eef2
Fesposito9197 <kekkoesp8@gmail.com>
2022-12-14 18:07:58 +0100

    refactoring

--- index.php
+++ index.php
@@ -59,44 +59,30 @@
        
         <div class="container">
             <table class="table">
-            
-            <tbody class="table-group-divider">
-                <tr>
-                <th scope="col">Name</th>
-                <?php foreach ($hotels as $hotel) { ?>
-                <td><?php echo $hotel["name"] ?></td>
-                <?php } ?>
-                </tr>
-                <tr>
-                <th scope="col">Description</th>
-                <?php foreach ($hotels as $hotel) { ?>
-                <td><?php echo $hotel["description"] ?></td>
-                <?php } ?>
-                </tr>
-                <tr>
-                <th scope="col">Parking</th>
-                <?php foreach ($hotels as $hotel) { ?>
-                <td><?php  echo $hotel["parking"] ? 'si' : 'no' ?></td>
-                <?php } ?>
-                </tr>
-                <tr>
-                <th scope="col">Vote</th>
-                <?php foreach ($hotels as $hotel) { ?>
-                <td><?php echo $hotel["vote"] ?></td>
-                <?php } ?>
-                </tr>
-                <tr>
-                <th scope="col">Distance to center</th>
-                <?php foreach ($hotels as $hotel) { ?>
-                <td><?php echo $hotel["distance_to_center"] ?></td>
-                <?php } ?>
-                </tr>
-            </tbody>
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Parking</th>
+                        <th scope="col">Vote</th>
+                        <th scope="col">Distance to center</th>
+                        </tr>
+                </thead>
+                <tbody class="table-group-divider">
+
+                    <?php foreach ($hotels as $hotel) { ?>
+                    <tr>
+                        <td><?php echo $hotel["name"] ?></td>
+                        <td><?php echo $hotel["description"] ?></td>
+                        <td><?php echo $hotel["parking"] ? 'si' : 'no' ?></td>
+                        <td><?php echo $hotel["vote"] ?></td>
+                        <td><?php echo $hotel["distance_to_center"] ?></td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
             </table>
+                
+
         </div>
-       
     </body>
-</html>
-                
-                
-                
+</html>          
